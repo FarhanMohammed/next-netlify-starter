1c99398a14135aca42b72294dac9be6d7b6af8c4
Farhan Mohammed <farhan.v4@gmail.com>
2023-11-06 23:55:17 -0500

    Update index.js
    snake game

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,52 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Snake from '../components/Snake';
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <div className="container">
+//       <Head>
+//         <title>Next.js Starter!</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main>
+//         <Header title="Welcome to my app!!" />
+//         <p className="description">
+//           Get started by editing <code>pages/index.js</code>
+//         </p>
+//       </main>
+
+//       <Footer />
+//     </div>
+//   )
+// }
+
+const Home = () => {
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Snake Game</title>
+        <meta name="description" content="Snake Game using Next.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1>Snake Game</h1>
+        <Snake />
       </main>
 
-      <Footer />
+      <footer>
+        <p>Powered by Next.js</p>
+      </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
+
+
+
+
+
